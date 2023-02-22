db115f16c092f7633ea141fe2f3a2ec9e934a26c
shirleyzero4 <shirley.c.nguyen@gmail.com>
2023-02-22 13:00:55 -0800

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import { AccessTime, AccountCircle, Search } from '@mui/icons-material'
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header-left'>
+            {/* Avatars for logged in user */}
+            <AccountCircle className='header-avatar'/>
+            {/* Time Icon */}
+            <AccessTime />
+        </div>
+
+        <div className='header-search'>
+            {/* Search Icon */}
+            <Search />
+            {/* Input */}
+            <input placeholder='Search'></input>
+        </div>
+
+        <div className='header-right'>
+            {/* Help Icon */}
+        </div>
+    </div>
+  )
+}
+
+export default Header
